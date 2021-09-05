68201dcddb94c36072e6c2f5c5ddaa6dda639b7c
sagit117 <sagit117@gmail.com>
2021-09-05 14:15:49 +0300

    types

--- index.d.ts
+++ index.d.ts
@@ -8,19 +8,19 @@ export default class EventEmitters {
      * @param eventName - имя события
      * @param callback - функция, которая будет вызвана по активации события
      */
-    public addListeners(eventName: string, callback: Function)
+    public addListeners(eventName: string, callback: Function): void
 
     /**
      * Сигнал к активации события
      * @param eventName - имя события
      * @param args - параметры
      */
-    public emit(eventName: string, ...args: any)
+    public emit(eventName: string, ...args: any): void
 
     /**
      * Удаление слушателя
      * @param eventName
      * @param callback
      */
-    public removeListener(eventName: string, callback: Function)
+    public removeListener(eventName: string, callback: Function): void
 }
